02df4068671344cb388f24e6aac66ba97deb3438
ogu83 <oguzkoroglu@gmail.com>
2019-04-18 13:22:57 +0300

    Login controller Disable_CertificateValidation

--- MyOtpDemoBankWeb/Api/LoginController.cs
+++ MyOtpDemoBankWeb/Api/LoginController.cs
@@ -1,5 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -25,9 +29,27 @@ namespace MyOtpDemoBankWeb.Api
             _clientFactory = clientFactory;
         }
 
+        //[Obsolete("Do not use this in Production code!!!", true)]
+        static void Disable_CertificateValidation()
+        {
+            // Disabling certificate validation can expose you to a man-in-the-middle attack
+            // which may allow your encrypted message to be read by an attacker
+            // https://stackoverflow.com/a/14907718/740639
+            ServicePointManager.ServerCertificateValidationCallback =
+                delegate (
+                    object s,
+                    X509Certificate certificate,
+                    X509Chain chain,
+                    SslPolicyErrors sslPolicyErrors
+                ) {
+                    return true;
+                };
+        }
+
         [HttpGet("GetOtp")]
         public async Task<string> GetOtp(string username)
         {
+            Disable_CertificateValidation();
             var url = $"{_otpOptions.Value.EndPoint}{_otpOptions.Value.Controller}{_otpOptions.Value.GetTOtpAction}?sharedKey={_otpOptions.Value.SharedKey}&userKey={username}";
             var client = _clientFactory.CreateClient();
             var response = await client.GetAsync(url);
@@ -44,6 +66,7 @@ namespace MyOtpDemoBankWeb.Api
         [HttpGet("ValidateOtp")]
         public async Task<bool> ValidateOtp(string username, string totp)
         {
+            Disable_CertificateValidation();
             var url = $"{_otpOptions.Value.EndPoint}{_otpOptions.Value.Controller}{_otpOptions.Value.ValidataTOtpAction}?sharedKey={_otpOptions.Value.SharedKey}&userKey={username}&totp={totp}";
             var client = _clientFactory.CreateClient();
             var response = await client.GetAsync(url);
@@ -70,6 +93,7 @@ namespace MyOtpDemoBankWeb.Api
         [HttpGet("IsAnyLoginRequest")]
         public bool IsAnyLoginRequest(string username)
         {
+            Disable_CertificateValidation();
             var result = _context.LoginRequests.Any(x => x.IsValidated == false && x.Username == username);
             return result;
         }
